2b4bdc7edb92db17ba591953b383363e29eb302a
Oecophyllaa <rangga.godzila@gmail.com>
2023-07-18 10:39:46 +0700

    update leaflet

--- resources/views/front/partials/navbar.blade.php
+++ resources/views/front/partials/navbar.blade.php
@@ -1,5 +1,5 @@
-<!-- Navigation-->
-<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+<!-- Navigation -->
+<nav class="navbar navbar-expand-lg bg-info text-uppercase fixed-top" id="mainNav">
   <div class="container">
     <a class="navbar-brand" href="#page-top">Siaga Banjir</a>
     <button class="navbar-toggler text-uppercase font-weight-bold bg-primary rounded text-white" type="button" data-bs-toggle="collapse"
@@ -9,8 +9,10 @@
     </button>
     <div class="navbar-collapse collapse" id="navbarResponsive">
       <ul class="navbar-nav ms-auto">
-        <li class="nav-item mx-lg-1 mx-0"><a class="nav-link px-lg-3 rounded py-3 px-0" href="#portfolio">Portfolio</a></li>
+        <li class="nav-item mx-lg-1 mx-0"><a class="nav-link px-lg-3 rounded py-3 px-0" href="#home">Beranda</a></li>
+        <li class="nav-item mx-lg-1 mx-0"><a class="nav-link px-lg-3 rounded py-3 px-0" href="#maps">Maps</a></li>
         <li class="nav-item mx-lg-1 mx-0"><a class="nav-link px-lg-3 rounded py-3 px-0" href="#about">About</a></li>
+        <li class="nav-item mx-lg-1 mx-0"><a class="nav-link px-lg-3 rounded py-3 px-0" href="#portfolio">Portfolio</a></li>
         <li class="nav-item mx-lg-1 mx-0"><a class="nav-link px-lg-3 rounded py-3 px-0" href="#contact">Contact</a></li>
       </ul>
     </div>
--- resources/views/front/partials/script.blade.php
+++ resources/views/front/partials/script.blade.php
@@ -2,4 +2,6 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
 <!-- Core theme JS-->
 <script src="https://cdn.jsdelivr.net/npm/startbootstrap-freelancer@7.0.7/dist/js/scripts.js"></script>
-<script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+{{-- <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script> --}}
+<!-- Leaflet -->
+<script src="https://cdn.jsdelivr.net/npm/leaflet@1.9.4/dist/leaflet.min.js"></script>
